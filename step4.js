ca4fb361e78a0abe2f8a2549b0c97fe870620695
Alexander Herta <alexanderherta2002@proton.me>
2024-12-17 13:45:32 +1100

    Change total based on which add-ons were chosen

--- step4.js
+++ step4.js
@@ -2,6 +2,13 @@ const totalHeading = document.querySelector(".total-heading-monthly-yearly");
 const totalHeadingPlan = document.querySelector(".total-heading-plan");
 const addZeros = document.querySelectorAll(".total-price-numbers-zero");
 const addTotalZeros = document.querySelector(".full-total-price-number-zero");
+const fullTotalPrice = document.querySelector(".full-total-price");
+const totalService = document.querySelector(".total-service");
+const totalStorage = document.querySelector(".total-storage");
+const totalProfile = document.querySelector(".total-profile");
+const totalArea = document.querySelector(".total-area");
+const totalOther = document.querySelector(".total-other");
+const hr = document.querySelector("hr");
 
 window.onload = function showTotals() {
   if(sessionStorage.getItem("isArcade") === "true") {
@@ -24,5 +31,53 @@ window.onload = function showTotals() {
       addZero.innerText = "0";
     });
     addTotalZeros.innerText = "0";
+  }
+
+  let addOnsCount = 0;
+  let totalValue = 140;
+
+  if(sessionStorage.getItem("serviceSelected") === "false") {
+    totalService.style.display = "none";
+    totalValue -= 10;
+  }
+  if(sessionStorage.getItem("storageSelected") === "false") {
+    totalStorage.style.display = "none";
+    totalValue -= 20;
+  }
+  if(sessionStorage.getItem("profileSelected") === "false") {
+    totalProfile.style.display = "none";
+    totalValue -= 20;
+  }
+
+  if(sessionStorage.getItem("serviceSelected") === "true") {
+    totalService.style.display = "flex";
+    addOnsCount += 1;
+  }
+  if(sessionStorage.getItem("storageSelected") === "true") {
+    totalStorage.style.display = "flex";
+    addOnsCount += 1;
+  }
+  if(sessionStorage.getItem("profileSelected") === "true") {
+    totalProfile.style.display = "flex";
+    addOnsCount += 1;
+  }
+
+  if(addOnsCount === 3) {
+    totalArea.style.height = "240px";
+    fullTotalPrice.innerText = `+${totalValue}/mo`;
+  }
+  if(addOnsCount === 2) {
+    totalArea.style.height = "190px";
+    fullTotalPrice.innerText = `+${totalValue}/mo`;
+  }
+  if(addOnsCount === 1) {
+    totalArea.style.height = "150px";
+    fullTotalPrice.innerText = `+${totalValue}/mo`;
+  }
+  if(addOnsCount === 0) {
+    totalArea.style.height = "100px";
+    fullTotalPrice.innerText = `+${totalValue}/mo`;
+    totalOther.remove();
+    hr.remove();
   }
 }
